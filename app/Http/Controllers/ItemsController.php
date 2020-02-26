632a9ee9156b3c816f6b32984b861fbe1b0e7298
unknown <jonsonyaanga@gmail.com>
2020-02-26 12:04:18 +0300

    second commit

--- app/Http/Controllers/ItemsController.php
+++ app/Http/Controllers/ItemsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Item;
+use Dotenv\Validator as DotenvValidator;
+use Validator;
 
 class ItemsController extends Controller
 {
@@ -36,7 +38,24 @@ class ItemsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator =Validator::make($request->all(),[
+            'text'=> 'required'
+
+        ]);
+        if($validator->fails()){
+            $response = array('response'=>$validator->messages(),'success'=>false);
+            return $response;
+
+        }{
+            $item = new Item;
+            $item->text = $request->input('text');
+            $item->body = $request->input('body');
+            $item->save();
+
+            return response()->json($item);
+
+
+        }
     }
 
     /**
@@ -71,7 +90,24 @@ class ItemsController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $validator =Validator::make($request->all(),[
+            'text'=> 'required'
+
+        ]);
+        if($validator->fails()){
+            $response = array('response'=>$validator->messages(),'success'=>false);
+            return $response;
+
+        }{
+            $item = Item::find($id);
+            $item->text = $request->input('text');
+            $item->body = $request->input('body');
+            $item->save();
+
+            return response()->json($item);
+
+
+        }
     }
 
     /**
